9ce3f2dfa73b0e584d91211f704d934f30add525
meekwind <chishui777@gmail.com>
2021-10-11 00:46:47 +0800

    页面控制器

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,10 @@
 <?php
+/**
+ * 转换路由名称为 css class
+ * @return string
+ */
+if (!function_exists('route_class')) {
+    function route_class() {
+        return str_replace('.', '-', Route::currentRouteName());
+    }
+}
